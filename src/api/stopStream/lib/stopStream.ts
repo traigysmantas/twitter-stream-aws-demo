c2618c0bbe6b8dde66ef73c33cd702cecbbee183
Mantas Traigys <Mantas.Traigys@ibm.com>
2022-05-25 09:29:47 +0300

    fixed stopStream fn

--- src/api/stopStream/lib/stopStream.ts
+++ src/api/stopStream/lib/stopStream.ts
@@ -1,11 +1,10 @@
 import { getStreamStatus } from 'common/services/dynamoDB/streams';
 import { isStreamRunning } from 'common/utils';
-import { handleError, LambdaHttpError } from 'common/utils/http';
+import { LambdaHttpError } from 'common/utils/http';
 
 import { sendCloseMessage } from './sendCloseMessage';
 
 export const stopStream = async (_, { dynamodb, sqs }) => {
-  try {
     const streamItem = await getStreamStatus(dynamodb);
 
     if (!isStreamRunning(streamItem)) {
@@ -16,8 +15,4 @@ export const stopStream = async (_, { dynamodb, sqs }) => {
     await sendCloseMessage(sqs);
 
     return { message: 'stream was stopped succesfully' };
-
-  } catch (err) {
-    return handleError(err);
-  }
 };
